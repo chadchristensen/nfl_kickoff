6d9b8ec6c66bdbe69bc86d80ee46fc0087287ec0
Chad Christensen <chadchristensen2@gmail.com>
2020-05-11 13:17:59 -0400

    update schedule for 2020

--- src/gameData.js
+++ src/gameData.js
@@ -3,224 +3,224 @@ export default {
     team: 'Atlanta Falcons',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/QNdwQPxtIRYUhnMBYq-bSA_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Seattle Seahawks',
+    kickoff: '09/13/2020 13:00:00'
   },
   'minnesota vikings': {
     team: 'Minnesota Vikings',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/Vmg4u0BSYZ-1Mc-5uyvxHg_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Green Bay Packers',
+    kickoff: '09/13/2020 13:00:00'
   },
   'washington redskins': {
     team: 'Washington Redskins',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/z_Or7w7bQ_ALmWUNsdd7AQ_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Philadelphia Eagles',
+    kickoff: '09/13/2020 13:00:00'
   },
   'philadelphia eagles': {
     team: 'Philadelphia Eagles',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/s4ab0JjXpDOespDSf9Z14Q_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Washington Redskins',
+    kickoff: '09/13/2020 13:00:00'
   },
   'dallas cowboys': {
     team: 'Dallas Cowboys',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/-zeHm0cuBjZXc2HRxRAI0g_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Los Angeles Rams',
+    kickoff: '09/13/2020 20:20:00'
   },
   'new york giants': {
     team: 'New York Giants',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/q8qdTYh-OWR5uO_QZxFENw_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Pittsburg Steelers',
+    kickoff: '09/14/2020 19:15:00'
   },
   'new england patriots': {
     team: 'New England Patriots',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/z89hPEH9DZbpIYmF72gSaw_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Miami Dolphins',
+    kickoff: '09/13/2020 13:00:00'
   },
   'pittsburg steelers': {
     team: 'Pittsburg Steelers',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/mdUFLAswQ4jZ6V7jXqaxig_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'New York Giants',
+    kickoff: '09/14/2020 19:15:00'
   },
-  'oakland raiders': {
-    team: 'Oakland Raiders',
+  'las vegas raiders': {
+    team: 'Las Vegas Raiders',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/QysqoqJQsTbiJl8sPL12Yg_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Carolina Panthers',
+    kickoff: '09/13/2020 13:00:00'
   },
   'denver broncos': {
     team: 'Denver Broncos',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/ZktET_o_WU6Mm1sJzJLZhQ_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Tennessee Titans',
+    kickoff: '09/14/2020 22:10:00'
   },
   'green bay packers': {
     team: 'Green Bay Packers',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/IlA4VGrUHzSVLCOcHsRKgg_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Minnesota Vikings',
+    kickoff: '09/13/2020 13:00:00'
   },
   'chicago bears': {
     team: 'Chicago Bears',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/7uaGv3B13mXyBhHcTysHcA_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Detroit Lions',
+    kickoff: '09/13/2020 13:00:00'
   },
   'seattle seahawks': {
     team: 'Seattle Seahawks',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/iVPY42GLuHmD05DiOvNSVg_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Atlanta Falcons',
+    kickoff: '09/13/2020 13:00:00'
   },
   'cincinnati bengals': {
     team: 'Cincinnati Bengals',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/wDDRqMa40nidAOA5883Vmw_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Los Angeles Chargers',
+    kickoff: '09/13/2020 16:05:00'
   },
   'san francisco 49ers': {
     team: 'San Francisco 49ers',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/ku3s7M4k5KMagYcFTCie_g_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Arizona Cardinals',
+    kickoff: '09/13/2020 16:25:00'
   },
   'tampa bay buccaneers': {
     team: 'Tampa Bay Buccaneers',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/cv5_3oTDQB0gT7l0g2gSGg_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'New Orleans Saints',
+    kickoff: '09/13/2020 16:25:00'
   },
   'cleveland browns': {
     team: 'Cleveland Browns',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/bTzlW33n9s53DxRzmlZXyg_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Baltimore Ravens',
+    kickoff: '09/13/2020 13:00:00'
   },
   'tennessee titans': {
     team: 'Tennessee Titans',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/9J9dhhLeSa3syZ1bWXRjaw_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Denver Broncos',
+    kickoff: '09/14/2020 22:10:00'
   },
   'kansas city chiefs': {
     team: 'Kansas City Chiefs',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/5N0l1KbG1BHPyP8_S7SOXg_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Houston Texans',
+    kickoff: '09/10/2020 10:20:00'
   },
   'jacksonville jaguars': {
     team: 'Jacksonville Jaguars',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/HLfqVCxzVx5CUDQ07GLeWg_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Indianapolis Colts',
+    kickoff: '09/13/2020 13:00:00'
   },
   'indianapolis colts': {
     team: 'Indianapolis Colts',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/zOE7BhKadEjaSrrFjcnR4w_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Jacksonville Jaguars',
+    kickoff: '09/13/2020 13:00:00'
   },
   'los angeles chargers': {
     team: 'Los Angeles Chargers',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/tmQHdnwuMcA9n69f5rBr0w_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Cincinnati Bengals',
+    kickoff: '09/13/2020 16:05:00'
   },
   'houston texans': {
     team: 'Houston Texans',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/sSUn9HRpYLQtEFF2aG9T8Q_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Kansas City Chiefs',
+    kickoff: '09/10/2020 10:20:00'
   },
   'new orleans saints': {
     team: 'New Orleans Saints',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/AC5-UEeN3V_fjkdFXtHWfQ_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Tampa Bay Buccaneers',
+    kickoff: '09/13/2020 16:25:00'
   },
   'buffalo bills': {
     team: 'Buffalo Bills',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/_RMCkIDTISqCPcSoEvRDhg_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'New York Jets',
+    kickoff: '09/13/2020 13:00:00'
   },
   'new york jets': {
     team: 'New York Jets',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/Rb4y9m3hkTcYVmdl10geqw_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Buffalo Bills',
+    kickoff: '09/13/2020 13:00:00'
   },
   'miami dolphins': {
     team: 'Miami Dolphins',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/1ysKnl7VwOQO8g94gbjKdQ_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'New England Patriots',
+    kickoff: '09/13/2020 13:00:00'
   },
   'baltimore ravens': {
     team: 'Baltimore Ravens',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/1vlEqqoyb9uTqBYiBeNH-w_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Cleveland Browns',
+    kickoff: '09/13/2020 13:00:00'
   },
   'arizona cardinals': {
     team: 'Arizona Cardinals',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/5Mh3xcc8uAsxAi3WZvfEyQ_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'San Francisco 49ers',
+    kickoff: '09/13/2020 16:25:00'
   },
   'detroit lions': {
     team: 'Detroit Lions',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/WE1l856fyyHh6eAbbb8hQQ_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Chicago Bears',
+    kickoff: '09/13/2020 13:00:00'
   },
   'los angeles rams': {
     team: 'Los Angeles Rams',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/UyYc_V_6Vabrvr7ous98_A_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Dallas Cowboys',
+    kickoff: '09/13/2020 20:20:00'
   },
   'carolina panthers': {
     team: 'Carolina Panthers',
     teamLogoUrl:
       'https://ssl.gstatic.com/onebox/media/sports/logos/4BdHvKq4Iyxsp8WaAbpDuQ_96x96.png',
-    opponent: '',
-    kickoff: ''
+    opponent: 'Las Vegas Raiders',
+    kickoff: '09/13/2020 13:00:00'
   }
 };
