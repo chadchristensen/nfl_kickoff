5d99c12cff81d5c57eda3a654a9ee79ec106c470
Chad Christensen <chadchristensen2@gmail.com>
2020-05-20 17:14:54 -0400

    create GameCard styled component

---
+++ src/components/GameCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components';
+
+import ConditionalComponent from './ConditionalComponent.js';
+import DateDisplay from './DateDisplay.js';
+import Matchup from './Matchup.js';
+
+const StyledGameCard = styled.div`
+
+`;
+
+export default function GameCard({ teamInfo, opponentInfo }) {
+  return (
+    <StyledGameCard>
+      <ConditionalComponent condition={typeof opponentInfo !== 'undefined'}>
+        <DateDisplay kickoff={teamInfo.kickoff} />
+      </ConditionalComponent>
+      <Matchup
+        teamInfo={teamInfo}
+        opponentInfo={opponentInfo}
+      />
+    </StyledGameCard>
+  )
+}
