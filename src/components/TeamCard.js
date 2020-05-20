b8bf1ce53f22b62749aaeb95a6ceb164207cae99
Chad Christensen <chadchristensen2@gmail.com>
2020-05-20 17:13:38 -0400

    create TeamCard styled component

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import styled from 'styled-components';
+
+const StyledCard = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 220px;
+  height: 220px;
+  padding: 1em 2.5em;
+  margin: 1em;
+  border: 1px solid #ddd;
+  background-color: #fff;
+  border-radius: 5px;
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+
+  @media(max-width: 820px) {
+    h2 {
+      font-size: 1em;
+    }
+    
+    img {
+      width: 45px;
+      height: 45px;
+    }
+  }
+`
 
 function TeamCard({teamName, teamLogoUrl}) {
   return (
     <AnimatePresence>
-      <motion.div
+      <StyledCard
         initial={{ scale: 0, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
         exit={{ scale: 0, opacity: 0}}
@@ -12,7 +43,7 @@ function TeamCard({teamName, teamLogoUrl}) {
       >
         <h2>{teamName}</h2>
         <img src={`${teamLogoUrl}`} alt={`${teamName} Logo`} />
-      </motion.div>
+      </StyledCard>
     </AnimatePresence>
   );
 }
