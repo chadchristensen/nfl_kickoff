7e77518db81b5d0d119b94346746458ac0c892ff
Chad Christensen <chadchristensen2@gmail.com>
2020-05-20 17:15:54 -0400

    convert ClockSection to use styled components

--- src/components/ClockSection.js
+++ src/components/ClockSection.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TimeValue = styled.div`
+  color: #ff0000;
+  text-shadow: 0px 0 5px #ec3636;
+  font-size: 5em;
+
+  @media(max-width: 820px) {
+    font-size: 3.5em;
+  }
+
+  @media(max-width: 425px) {
+    font-size: 2.5em;
+  }
+`;
+
+const TimeLabel = styled.div`
+  font-weight: bold;
+`
 
 export default function ClockSection({ value, label }) {
   return (
     <div>
-      <div className="time-value">{value}</div>
-      <div className="time-label">{label.toUpperCase()}</div>
+      <TimeValue>{value}</TimeValue>
+      <TimeLabel>{label.toUpperCase()}</TimeLabel>
     </div>
   );
 }
