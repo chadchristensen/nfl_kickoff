08183fd7251087f0290673f2836f5e3c8fc113f2
Chad Christensen <chadchristensen2@gmail.com>
2020-05-20 17:16:18 -0400

    create Countdown styled component

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import h from '../helpers';
 import ClockSection from './ClockSection.js';
+import styled from 'styled-components';
+
+const CountdownTimer = styled.div`
+  font-family: 'digital-7', sans-serif;
+  background-color: #222;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  color: #eee;
+  border-radius: 2px;
+
+  & > div {
+    padding: 1em;
+    width: calc(100% / 4);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`
 
 export default function Countdown({ kickoff }) {
   return (
-    <div className="countdown-timer">
+    <CountdownTimer>
       <ClockSection value={h.getDaysFromMilliseconds(kickoff)} label="days" />
       <ClockSection value={h.getHoursFromMilliseconds(kickoff)} label="hours" />
       <ClockSection
@@ -15,6 +35,6 @@ export default function Countdown({ kickoff }) {
         value={h.getSecondsFromMilliseconds(kickoff)}
         label="seconds"
       />
-    </div>
+    </CountdownTimer>
   );
 }
