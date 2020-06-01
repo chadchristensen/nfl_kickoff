25fb9757b4d22bbc49259744bf1609f00fc79a4a
Chad Christensen <chadchristensen2@gmail.com>
2020-06-01 13:21:39 -0400

    extract typeahead input to its own component

---
+++ src/components/TypeaheadInput.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const SearchWrapper = styled.div`
+  z-index: 101;
+  display: flex;
+  align-items: center;
+  padding: 0 0.25rem;
+  background-color: white;
+  border-radius: inherit;
+  border-bottom-left-radius: ${props => props.areResultsVisible ? '0' : 'inherit'};
+  border-bottom-right-radius: ${props => props.areResultsVisible ? '0' : 'inherit'};
+
+  > svg {
+    padding: 1rem;
+    stroke: #4E4E4E;
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+`
+
+const SearchInput = styled.input`
+  width: 100%;
+  padding: 0.75rem;
+  font-size: 1.25rem;
+  appearance: none;
+  border: none;
+`
+
+export default function TypeaheadInput({ handleTeamSearch, searchTerm }) {
+  return (
+    <SearchWrapper areResultsVisible={searchTerm.length > 1}>
+      <SearchInput
+        type="search"
+        name="team-search"
+        id="team-search"
+        aria-label="Search for a team"
+        placeholder="Search for a team"
+        onChange={handleTeamSearch}
+        value={searchTerm}
+      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#000000"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <circle cx="11" cy="11" r="8" />
+        <line x1="21" y1="21" x2="16.65" y2="16.65" />
+      </svg>
+    </SearchWrapper>
+  )
+}
