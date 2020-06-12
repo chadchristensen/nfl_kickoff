01dc77e3bb35356f39270515d09e33dc1921fe9c
Chad Christensen <chadchristensen2@gmail.com>
2020-06-12 15:07:24 -0400

    add content backdrop effect

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Content = styled.div`
+  position: relative;
+
+  &:after {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 0;
+      height: 150px;
+      width: 100%;
+      background-image: url("./img/grass.jpg");
+      z-index: 0;
+    }
+`
+
 const StyledWrapper = styled.div`
-  background-color: rgba(0, 0, 0, 0.25);
-  margin: 4em;
-  padding: 1.5em 3em;
+  position: relative;
+  z-index: 1;
+  background-color: #FFFFFF;
+  box-shadow: 0 2px 9px rgba(0, 0, 0, 0.25);
+  width: 75%;
+  margin: 0 auto;
+  padding: 1.5em 2.5em;
   border-radius: 5px;
 `
 
 export default function Wrapper({ children }) {
   return (
-    <StyledWrapper>
-      { children }
-    </StyledWrapper>
+    <Content>
+      <StyledWrapper>
+        { children }
+      </StyledWrapper>
+    </Content>
   )
 }
